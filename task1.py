709efddfb8c62b7d9c459aee12383f83b181115b
infinious <85552157+infinious@users.noreply.github.com>
2021-06-13 18:14:54 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,42 @@
+#task-1 #day-1
+#1
+print("30 days 30 hours challenge")
+print('30 days 30 hours challenge')
+#2
+days="thirty"
+print(days)
+#3
+football="ronaldo"
+print(football[3])
+#4
+challenge="i will win"
+print(challenge[7:10])
+#5
+challenge="i will win"
+print(len(challenge))
+#6
+cricket="VIRAT"
+print(cricket.lower())
+#7
+a="30 days"
+b="30 hours challenge"
+c=a+b
+print(c)
+#8
+a="30 days"
+b="30 hours"
+c=a+" "+b
+print(c)
+#9
+text="thirty days thirty hours"
+x=text.isalnum()
+print(x)
+text="thirty days thirty hours"
+x=text.capitalize()
+print(x)
+text="30 days thirty hours"
+x=text.isalpha()
+print(x)
+text="thirty days thirty hours"
+x=text.find("days")
+print(x)
