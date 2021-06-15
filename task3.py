80d2808f6b306aa6a91a915db70aa2f1dd558151
infinious <85552157+infinious@users.noreply.github.com>
2021-06-15 18:14:35 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,17 @@
+#1
+a=b=c=1000
+print("a=",a,"b=",b,"c=",c)
+print("divide a by 90:",a/90)
+print("mutliply b by 50:",a*50)
+print("add c buy 10:",b+10)
+#2
+football = ("rosaldo")
+r = (football.replace("s","n"))
+print(r)
+#3
+a = 10
+b = 5.3
+x = int(a+b)
+print(x)
+y = float(a+b)
+print(y)
