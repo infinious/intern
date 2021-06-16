b1296fb034bd702fabad6491a13a01907640b930
infinious <85552157+infinious@users.noreply.github.com>
2021-06-16 18:34:51 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,19 @@
+#1
+A =[40,987,6758,928375,84758959]
+A.append(90897)
+print(A)
+
+n= 1000
+re = [x for x in range (0,n)]
+print(max(re))
+print(min(re))
+del re[1:987]
+print(re)
+#2
+a = ("messi","neymar","ronaldo","benzema")
+x= reversed(a)
+print(tuple(x))
+#3
+m = ("virat","dhoni","shami","jadeja")
+l = list(m)
+print(l)
