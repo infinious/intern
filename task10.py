eb4ee99c0ad101c227e886a3c933613fba0e13b7
infinious <85552157+infinious@users.noreply.github.com>
2021-06-24 00:42:43 +0530

    Add files via upload

---
+++ task10.py
@@ -0,0 +1,19 @@
+import mymodule as m
+m.mymodule("tushar")
+import pandas as pd
+print("installed pandas version:",pd.__version__)
+
+
+import random as r
+print("random number between 1 to 100 :",r.randint(1,100))
+
+
+import sys
+for p in sys.path:
+    print(p)
+
+
+    def mymodule(var):
+        vkk = ["jgfgj", "rujhf", "okjyf", 45, 26, 78]
+        vkk.append(var)
+        print(vkk)
